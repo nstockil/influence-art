1df0cef93a94e9b2e0e0cca1f92fba53b2f15c56
Niamh Stockil <niamhstockil@gmail.com>
2018-08-06 17:36:58 +0100

    adding base files and main script with timer

---
+++ influence-art.py
@@ -0,0 +1,18 @@
+import time
+
+#HELPER METHODS
+def set_up():
+    print("Set up")
+
+
+def create_art():
+    print("Creating Art")
+
+# EXECUTION CODE
+set_up()
+
+while True:
+    # set timer
+    time.sleep(10)
+    # create art
+    create_art()
